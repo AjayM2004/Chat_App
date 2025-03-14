0b3b4fc58786d0af361be7d5696dbc00795ed7bc
AjayM2004 <ajay97908940@gmail.com>
2025-03-14 23:20:34 +0530

    Adding backend files

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,31 +1,15 @@
 import express from 'express';
+import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js";
-
-
-
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3007;
-
+const PORT = process.env.PORT;
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
 
-const router = express.Router();
-
-router.post("/signup", (req, res) => {
-  res.send(`Signup`);
-});
-
-router.post("/login", (req, res) => {
-  res.send(`Login`);
-});
-
-router.post("/logout", (req, res) => {
-  res.send("Logout");
-});
-
-export default router;
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,17 +1,12 @@
 import express from 'express';
 
-
+import { signup,logout,login } from '../controllers/auth.controller.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
-
-
-app.use("/api/auth", authRoutes);
-
-
-
 const router = express.Router();
 
-
+router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
 export default router;
